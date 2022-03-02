6c4eb1d2b3dec7e48b84939b6210857e7ccae8cb
Amit <abhatna7@stevens.edu>
2022-03-02 12:29:11 -0500

    linting/coverage adjustments, incremented commit count for this repo in first test case

--- hw04a.py
+++ hw04a.py
@@ -1,15 +1,29 @@
+"""
+Provides the function get_repo_list (which
+uses the GitHub API) below, and lists the
+repo data along with the number of commits,
+with each line following the form
+Project: 5
+for a repository called "Project" with 5 commits
+"""
 import requests
-import json
 
-def get_repo_list(ID):
-    repo_data = requests.get(f'https://api.github.com/users/{ID}/repos')
+
+def get_repo_list(github_id):
+    """
+    Given a GitHub user ID with public repositories, the function
+    returns an array of tuples of form (repo, n) where repo is a
+    string referring to the name of the public repository and n
+    is the number of commits for that repository.
+    """
+    repo_data = requests.get(f'https://api.github.com/users/{github_id}/repos')
     arr = []
     if repo_data:
         repo_json = repo_data.json()
         for repo in repo_json:
             repo_name = repo['name']
             commits_data = requests.get(
-                f'https://api.github.com/repos/{ID}/{repo_name}/commits')
+                f'https://api.github.com/repos/{github_id}/{repo_name}/commits')
             if commits_data:
                 commits_json = commits_data.json()
                 arr.append((repo_name, len(commits_json)))
@@ -19,11 +33,12 @@ def get_repo_list(ID):
     else:
         return f"Error retrieving repo data with status code {repo_data.status_code}: {repo_data.reason}"
 
+
 if __name__ == '__main__':
-    userId = "nickopicz"
-    result = get_repo_list(userId)
+    USER_ID = "amitb913"
+    result = get_repo_list(USER_ID)
     if not isinstance(result, list):
         print(result)
     else:
-        for repo in result:
-            print(repo[0] + ":", repo[1])
+        for repository in result:
+            print(repository[0] + ":", repository[1])
--- test_hw04.py
+++ test_hw04.py
@@ -1,15 +1,38 @@
+"""Suite of test cases for getting the list of repos"""
 import unittest
 from hw04a import get_repo_list
 
 
-class Test_GetRepoList(unittest.TestCase):
+class TestGetRepoList(unittest.TestCase):
+    """Class representing the suite of test cases for getting the list of repos"""
+
     def test_correct_response(self):
-        correct_response = [('electron-app-iot', 6), ('SSW345-hw1', 3), ('SSW345-lab1', 1), ('SSW567-hw0', 2),
-                            ('SSW567-hw1', 7), ('SSW567-hw2a', 11), ('SSW567-HW4a', 6), ('Superalgos', 30)]
+        """
+        Tests the correct (expected) response. Keep in mind
+        this test may need to be adjusted based on whether the
+        user in question makes changes to their repositories
+        (either creating/renaming/deleting repos, committing changes)
+        """
+        correct_response = [('electron-app-iot', 6), ('SSW345-hw1', 3),
+                            ('SSW345-lab1', 1), ('SSW345-REST', 2),
+                            ('SSW567-hw0', 2), ('SSW567-hw1',
+                                                7), ('SSW567-hw2a', 11),
+                            ('SSW567-HW4a', 7), ('Superalgos', 30)]
         self.assertEqual(get_repo_list("amitb913"), correct_response)
+
     def test_nonexistent_user(self):
-        userId = "sdiofjasdiofjasdiofjsdafunsuadofn"
-        self.assertEqual(get_repo_list(userId), "Error retrieving repo data with status code 404: Not Found")
+        """Tests the response for a username
+        that does not exist on GitHub"""
+        user_id = "sdiofjasdiofjasdiofjsdafunsuadofn"
+        self.assertEqual(get_repo_list(
+            user_id), "Error retrieving repo data with status code 404: Not Found")
+
+    def test_no_commits(self):
+        """Tests the response for a user
+        with a repository is empty/has 0 commits"""
+        self.assertEqual(get_repo_list(
+            "brendanprobst"), "Error retrieving commit data from repo 'ssw215' with status code 200: OK")
+
 
 if __name__ == '__main__':
     unittest.main()
